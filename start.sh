28366fd345c1d40216698049a532fb77df3e1676
p151541 <jobkim0410@gmail.com>
2021-06-14 13:38:25 +0900

    start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
-#sudo docker build -t nginx-admin-react:0.1 .
+sudo docker build -t nginx-admin-react:0.1 .
+sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter="name=coffee-factory-admin-stg" --format="{{.ID}}"))
 sudo docker run -d --name coffee-factory-admin-stg -p 80:80 nginx-admin-react:0.1
